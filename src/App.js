bbbca1ee40e3e0add0914d8af7f1dd5fb497022b
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2024-08-03 14:25:30 +0300

    A modal and icon button were added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import Container from "./components/Container";
 import TodoList from "./components/TodoList";
 import TodoEditor from "./components/TodoEditor";
 import initialTodos from "./todos.json";
+import IconButton from "./components/IconButton";
+import Modal from "./components/Modal";
+// import "";
 // import { format } from "date-fns";
 
 // const now = format(new Date(), "dd.MM.yyyy");
 
 class App extends Component {
 	state = {
-		todos: [],
+		todos: initialTodos,
+		filter: "",
+		showModal: false,
 	};
+
+	componentDidMount() {
+		const todos = localStorage.getItem("todos");
+		const parsedTodos = JSON.parse(todos);
+
+		if (parsedTodos) {
+			this.setState({ todos: parsedTodos });
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.todos !== this.state.todos) {
+			localStorage.setItem("todos", JSON.stringify(this.state.todos));
+		}
+	}
 
 	addTodo = (text) => {
 		const todo = {
@@ -48,18 +68,46 @@ class App extends Component {
 		);
 	};
 
+	toggleModal = () => {
+		this.setState(({ showModal }) => ({ showModal: !showModal }));
+	};
+
 	render() {
-		const { todos } = this.state;
+		const { todos, filter, showModal } = this.state;
 		// const date = format(new Date(), "dd.MM.yyyy");
 		const totalTodoCount = todos.length;
 		const completedTodoCount = this.calculateCompletedTodos();
 		return (
 			<Container>
+				<button type="button" onClick={this.toggleModal}>
+					Відкрити модалку
+				</button>
+				{/* <IconButton onClick={this.toggleModal}>hello</IconButton> */}
+				{showModal && (
+					<Modal onClose={this.toggleModal}>
+						<h1>Hello</h1>
+						<p>
+							Veniam in ex aliquip sit proident aliqua minim ea fugiat proident
+							officia est velit. Incididunt nisi commodo duis cupidatat
+							voluptate exercitation consequat officia fugiat. Sint esse
+							occaecat dolor velit consectetur commodo pariatur incididunt est.
+							Sit nulla cillum adipisicing cupidatat excepteur aliqua ullamco
+							aliqua nostrud.
+						</p>
+						<button type="button" onClick={this.toggleModal}>
+							Закрити модалку
+						</button>
+					</Modal>
+				)}
+
 				<h1>Завдання</h1>
 				<div>
 					<p>Всього нотаток: {totalTodoCount}</p>
 					<p>Виконано: {completedTodoCount}</p>
 				</div>
+
+				{/* <IconButton>hello</IconButton> */}
+
 				<TodoEditor onSubmit={this.addTodo}></TodoEditor>
 
 				<TodoList
